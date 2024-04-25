731652273a92904f75469a3ce81eb88d8285d292
anguswg-ucsb <anguswatters@gmail.com>
2024-04-25 14:56:50 -0700

    reduced number of calls to ingredient_slicer, added code to keep all branded foods ID, added food_attribute data to foundation foods and sr legacy, added a constants file for converting some meat units to human readable food categories

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import argparse
 import os
+import re
+import json
 import pandas as pd
+import numpy as np
 
-from preprocessing._utils import get_usda_urls
-from preprocessing._utils import fillna_and_define_dtype
+from preprocessing._utils import get_usda_urls, dict_to_json, fillna_and_define_dtype, postprocess_stacked_df, fillna_and_set_dtypes
 
 from preprocessing.process_foundation import process_foundation
 from preprocessing.process_srlegacy import process_srlegacy
 from preprocessing.process_branded import process_branded
 
+# ---------------------------------------------------------------------
+# ---- Parse command-line arguments ----
+# ---------------------------------------------------------------------
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='download and process USDA Food Data Central datasets.')
@@ -17,40 +22,47 @@ parser.add_argument('--filename', default='usda_food_nutrition_data.parquet', he
 parser.add_argument('--keep_files', action='store_true', help='keep raw/indv files post-processing (default: delete raw/indv files)')
 args = parser.parse_args()
 
-
 # Check filename and extension
 filename = args.filename
 file_ext = filename.split('.')[-1] if '.' in filename else None
 filename = filename if file_ext else f"{filename}.csv"
-
 
 # Check keep_files flag
 keep_files = args.keep_files
 if keep_files:
     print(f"\n'keep_files' flag specified:\n> Raw and individual files will be kept after processing.")
 
+# ---------------------------------------------------------------------
+# ---- Setup raw and output directories ----
+# ---------------------------------------------------------------------
 
 # Define directories and ensure they exist (raw_dir existence will be checked in individual processing files)
 OUTPUT_DIR = args.output_dir
 RAW_DIR = os.path.join(OUTPUT_DIR, 'FoodData_Central_raw')
 print(f'\nInitializing processing of USDA FDC data. Output directory set to:\n> {OUTPUT_DIR}\n')
 
-
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
     print(f'Directory created:\n> {OUTPUT_DIR}\n')
 
+# ---------------------------------------------------------------------
+# ---- Process USDA datasets ----
+# ---------------------------------------------------------------------
 
 # Gather urls and send them out to appropriate processing scripts
 usda_urls = get_usda_urls()
 
 foundation_urls = [url for url in usda_urls if 'foundation' in url or 'FoodData_Central_csv' in url]
-srlegacy_url = [url for url in usda_urls if 'sr_legacy' in url][0]
-branded_url = [url for url in usda_urls if 'branded' in url][0]
+srlegacy_url    = [url for url in usda_urls if 'sr_legacy' in url][0]
+branded_url     = [url for url in usda_urls if 'branded' in url][0]
 
 process_foundation(foundation_urls, OUTPUT_DIR, RAW_DIR, keep_files)
 process_srlegacy(srlegacy_url, OUTPUT_DIR, RAW_DIR, keep_files)
 process_branded(branded_url, OUTPUT_DIR, RAW_DIR, keep_files)
+
+# ---------------------------------------------------------------------
+# ---- Postprocess processed USDA datasets ----
+# ---------------------------------------------------------------------
 
 print(f'Initializing stacking of individually processed data:')
 for root, dirs, files in os.walk(OUTPUT_DIR):
@@ -58,35 +70,23 @@ for root, dirs, files in os.walk(OUTPUT_DIR):
         if '.parquet' in file:
             print(f'> {file}')
 
-
 # Stack processed data, reorder columns, and save csv
 stacked_data = pd.concat([
     pd.read_parquet(os.path.join(OUTPUT_DIR, 'processed_foundation.parquet')),
     pd.read_parquet(os.path.join(OUTPUT_DIR, 'processed_srlegacy.parquet')),
     pd.read_parquet(os.path.join(OUTPUT_DIR, 'processed_branded.parquet'))
 ])
-stacked_data.reset_index(drop=True, inplace=True)
 
-stacked_data = stacked_data[[
-                        'fdc_id', 'usda_data_source', 'data_type', 'category', 'brand_owner', 'brand_name', 'food_description', 'ingredients', 
-                        'portion_amount', 'portion_unit', 'portion_modifier', 'std_portion_amount', 'std_portion_unit', 'portion_gram_weight', 
-                        'portion_energy', 'energy', 'carbohydrate_by_difference', 'protein', 'total_lipid_fat', 'fiber_total_dietary', 'sugars_total', 
-                        'calcium_ca', 'iron_fe', 'vitamin_c_total_ascorbic_acid', 'vitamin_a_rae', 'vitamin_e_alphatocopherol', 
-                        'sodium_na', 'cholesterol', 'fatty_acids_total_saturated', 'fatty_acids_total_trans', 'fatty_acids_total_monounsaturated', 
-                        'fatty_acids_total_polyunsaturated', 'vitamin_k_phylloquinone', 'thiamin', 'riboflavin', 'niacin', 'vitamin_b6', 'folate_total', 
-                        'vitamin_b12', 'vitamin_d3_cholecalciferol', 'vitamin_d2_ergocalciferol', 'pantothenic_acid', 'phosphorus_p', 'magnesium_mg', 
-                        'potassium_k', 'zinc_zn', 'copper_cu', 'manganese_mn', 'selenium_se', 'carotene_beta', 'retinol', 'vitamin_k_dihydrophylloquinone', 
-                        'vitamin_k_menaquinone4', 'tryptophan', 'threonine', 'methionine', 'phenylalanine', 'tyrosine', 'valine', 'arginine', 'histidine', 
-                        'isoleucine', 'leucine', 'lysine', 'cystine', 'alanine', 'glutamic_acid', 'glycine', 'proline', 'serine', 'sucrose', 'glucose', 
-                        'maltose', 'fructose', 'lactose', 'galactose', 'choline_total', 'betaine'
-                    ]]
+# apply some post-processing 
+# (select columns, fillna, set dtypes, fill in missing food_common_name and food_common_category values by food_description groups)
+stacked_data = postprocess_stacked_df(stacked_data)
 
-for col in stacked_data.columns.tolist():
-    fillna_and_define_dtype(stacked_data, col)
+# ---------------------------------------------------------------------
+# ---- Save final output data and cleanup directories ----
+# ---------------------------------------------------------------------
 
 stacked_data.to_csv(os.path.join(OUTPUT_DIR, filename), index=False)
-
-
+# stacked_data.to_parquet(os.path.join(OUTPUT_DIR, "usda_food_nutrition_data2.parquet"), index=False)
 
 # Delete raw dir/individual processed files if keep_files flag is not specified
 for root, dirs, files in os.walk(OUTPUT_DIR):
@@ -106,4 +106,3 @@ for root, dirs, files in os.walk(OUTPUT_DIR):
 
 
 print(f"\nProcessing of USDA FDC data is complete. The processed data file ('{filename}') is now available in:\n> {OUTPUT_DIR}\n")
-
